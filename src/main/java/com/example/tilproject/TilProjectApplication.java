0845aa708c81887f8ef301a868b807643d107258
Ohjinn <hjin9445@gmail.com>
2021-12-07 22:13:06 +0900

    211207

--- src/main/java/com/example/tilproject/TilProjectApplication.java
+++ src/main/java/com/example/tilproject/TilProjectApplication.java
@@ -1,5 +1,6 @@
 package com.example.tilproject;
 
+import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.builder.SpringApplicationBuilder;
 import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
@@ -9,14 +10,9 @@ import org.springframework.scheduling.annotation.EnableScheduling;
 @EnableJpaAuditing
 @EnableScheduling
 public class TilProjectApplication {
-    public static final String APPLICATION_LOCATIONS = "spring.config.location="
-            + "classpath:application.yml,"
-            + "classpath:aws.yml";
 
     public static void main(String[] args) {
 
-        new SpringApplicationBuilder(TilProjectApplication.class)
-                .properties(APPLICATION_LOCATIONS)
-                .run(args);
+        SpringApplication.run(TilProjectApplication.class, args);
     }
 }
